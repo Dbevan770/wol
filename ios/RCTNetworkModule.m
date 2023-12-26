5eed854a0161e5c96786966e0da20f0974440384
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-12-26 23:17:59 +0100

    Add RCTNetworkModule files and bridging headers

---
+++ ios/RCTNetworkModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTNetworkModule.m
+//  wol
+//
+//  Created by Danny on 12/22/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/RCTNetworkModule.swift
@@ -0,0 +1,8 @@
+//
+//  RCTNetworkModule.swift
+//  wol
+//
+//  Created by Danny on 12/22/23.
+//
+
+import Foundation
